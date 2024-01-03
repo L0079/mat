2e2a941466d27dfd453b92cb2e615f3aecd36fa7
L0079 <luigi.porro.0@gmail.com>
2024-01-03 17:10:38 +0100

    little bug fixing in order to deploy

--- src/pages/PayableInvoices.jsx
+++ src/pages/PayableInvoices.jsx
@@ -1,7 +1,7 @@
 import Heading from "../ui/Heading";
 import Row from "../ui/Row";
 import PayableInvoicesTable from "../features/payableInvoices/PayableInvoicesTable";
-import PayableInvoiceOperations from "../features/PayableInvoices/PayableInvoiceOperations";
+import PayableInvoiceOperations from "../features/payableInvoices/PayableInvoiceOperations";
 
 function PayableInvoices() {
   return (
